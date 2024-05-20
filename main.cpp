b4d7e38e5b1c2179f8107d2c48953b7da3a00d09
BiKend <bikend.learn@gmail.com>
2024-05-20 10:47:41 +0400

    Добавлена проверка на некорректные значения длины и количества полос дороги при чтении данных из файла

--- main.cpp
+++ main.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <stdexcept> // Для исключений
+#include <stdexcept>
 
-// Класс дороги
 class Road {
 private:
-  int length; // Длина дороги в метрах
-  int lanes; // Количество полос на дороге
+  int length;
+  int lanes;
 
 public:
-  // Конструктор без параметров
   Road() : length(0), lanes(0) {}
 
-  // Метод для чтения данных из файла
   void readDataFromFile(const std::string& fileName) {
     std::ifstream inputFile(fileName);
-    if (inputFile.is_open()) {
-      if (!(inputFile >> length >> lanes)) {
-        throw std::runtime_error("Ошибка чтения данных из файла.");
-      }
-      inputFile.close();
-    } else {
+    if (!inputFile.is_open()) {
       throw std::runtime_error("Не удалось открыть файл: " + fileName);
     }
+    if (!(inputFile >> length >> lanes) || length <= 0 || lanes <= 0) {
+      throw std::runtime_error("Некорректные данные в файле: " + fileName);
+    }
+    inputFile.close();
   }
 
-  // Метод для вывода значений полей
   void printValues() {
-    std::cout << "Длина дороги: " << length << " метров" << std::endl;
-    std::cout << "Количество полос: " << lanes << std::endl;
+    std::cout << "Длина дороги: " << length << " метров\n";
+    std::cout << "Количество полос: " << lanes << '\n';
   }
 };
 
@@ -37,13 +32,10 @@ int main() {
   Road road;
 
   try {
-    // Чтение данных из файла
     road.readDataFromFile("input.txt");
-
-    // Вывод значений полей
     road.printValues();
   } catch(const std::exception& e) {
-    std::cerr << "Ошибка: " << e.what() << std::endl;
+    std::cerr << "Ошибка: " << e.what() << '\n';
     return 1;
   }
 
